c997416fe2fa4419b3419d1482e0d6ccba5c6fd0
Igor Petrov <petrovswd@gmail.com>
2024-10-14 20:27:05 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HappyArrayProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100000; i++)
+            {
+                Console.WriteLine("Hello, World! " + i);
+            }
+            
+        }
+    }
+}
